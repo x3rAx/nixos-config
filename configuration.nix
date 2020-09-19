3ea978c2f918f3413a036243e4796261fddd49d6
^x3ro <git@x3ro.dev>
2020-09-19 10:59:01 +0200

    👌 IMPROVE: Configure locales

--- configuration.nix
+++ configuration.nix
@@ -58,8 +58,20 @@
   i18n = {
     # consoleFont = "Lat2-Terminus16";
     # consoleKeyMap = "us";
-    # defaultLocale = "en_US.UTF-8";
-    consoleUseXkbConfig = true;
+    supportedLocales = [
+    	"en_US.UTF-8/UTF-8"
+    	"de_DE.UTF-8/UTF-8"
+    ];
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      # LANG is set by `i18n.defaultLocale`
+      # LC_ALL = (unset)
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_COLLATE = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
+    };
   };
 
   # Set your time zone.
