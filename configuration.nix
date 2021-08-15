59f2a7b991fb3445d0b9055aaa9587a0026ad0a6
^x3ro <git@x3ro.dev>
2021-08-15 12:34:50 +0200

    👌 IMPROVE: Temporarily disable nvidia graphics

--- configuration.nix
+++ configuration.nix
@@ -295,17 +295,16 @@ in
   # Update Intel microcode
   hardware.cpu.intel.updateMicrocode = true;
 
-  # Use the new Gallium `iris` driver for Intel graphics
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia.prime = {
-    offload.enable = true;
+  #services.xserver.videoDrivers = [ "nvidia" ];
+  #hardware.nvidia.prime = {
+  #  offload.enable = true;
 
-    # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
-    intelBusId = "PCI:0:2:0";
+  #  # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+  #  intelBusId = "PCI:0:2:0";
 
-    # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
-    nvidiaBusId = "PCI:1:0:0";
-  };
+  #  # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+  #  nvidiaBusId = "PCI:1:0:0";
+  #};
 
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
