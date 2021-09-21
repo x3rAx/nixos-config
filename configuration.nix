155dbe7623ff18419229d836b57caa4cb76721c5
^x3ro <git@x3ro.dev>
2021-09-21 09:14:26 +0200

    📦 NEW: Add example for automatic garbage collection

--- configuration.nix
+++ configuration.nix
@@ -444,6 +444,12 @@ in
     builders-use-substitutes = true
   '';
 
+  #nix.gc = {
+  #  automatic = true;
+  #  dates = "weekly";
+  #  options = "--delete-older-than 30d";
+  #}
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
