6a4a475e2048a8b527dc4e6273e5044a6b11c843
^x3ro <git@x3ro.dev>
2022-02-23 11:03:51 +0100

    👌 IMPROVE: nvim: Add system config

--- roles/common.nix
+++ roles/common.nix
@@ -23,6 +23,37 @@
         EDITOR = "vim";
     };
 
+    environment.etc."xdg/nvim/sysinit.vim" = {
+      text = ''
+        " Indent with 4 spaces
+            filetype indent on
+            set tabstop=4    " show existing tabs with 4 spaces width
+            set shiftwidth=4 " when indenting with '>', use 4 spacees width
+            set expandtab    " on pressing tab, inssert 4 spaces
+            set autoindent
+            autocmd FileType yaml set indentkeys-=0#
+
+        " Jump to last position
+            autocmd BufReadPost *
+                \ if line("'\"") >= 1 && line("'\"") <= line("$") |
+                \   exe "normal! g`\"" |
+                \ endif
+
+        " Smart case (in)sensitive search
+            :set ignorecase
+            :set smartcase
+
+        " Scrolloff
+            set scrolloff=5
+            " When jumping to end of file, leave 5 lines blank below
+            nnoremap G 5<C-y>G5<C-e>
+
+        " Improved line shifting
+            vnoremap < <gv
+            vnoremap > >gv
+      '';
+    };
+
     environment.etc."gitconfig" = {
         mode = "0644";
         text = ''
