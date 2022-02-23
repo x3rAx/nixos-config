f66024ea85f3c4d00ca8ab51202265d5bd33f8d6
^x3ro <git@x3ro.dev>
2022-02-23 11:45:06 +0100

    🐛 FIX: neovim: Make system config available when using `sudo nvim`

--- roles/common.nix
+++ roles/common.nix
@@ -1,13 +1,48 @@
 # Stuff that really should be on all machines
 { config, pkgs, ... }:
 
-{
+let
+  neovim-system-config = pkgs.writeTextFile rec{
+    name = "neovim-system-config";
+    destination = "/etc/xdg/nvim/sysinit.vim";
+    text = ''
+      " Indent with 4 spaces
+          filetype indent on
+          set tabstop=4    " show existing tabs with 4 spaces width
+          set shiftwidth=4 " when indenting with '>', use 4 spacees width
+          set expandtab    " on pressing tab, inssert 4 spaces
+          set autoindent
+          autocmd FileType yaml set indentkeys-=0#
+
+      " Jump to last position
+          autocmd BufReadPost *
+              \ if line("'\"") >= 1 && line("'\"") <= line("$") |
+              \   exe "normal! g`\"" |
+              \ endif
+
+      " Smart case (in)sensitive search
+          :set ignorecase
+          :set smartcase
+
+      " Scrolloff
+          set scrolloff=5
+          " When jumping to end of file, leave 5 lines blank below
+          nnoremap G 5<C-y>G5<C-e>
+
+      " Improved line shifting
+          vnoremap < <gv
+          vnoremap > >gv
+    '';
+  };
+in {
     # List packages installed in system profile. To search, run:
     # $ nix search wget
     environment.systemPackages = with pkgs; [
         nix-bash-completions
 
         (neovim.override { vimAlias = true; })
+        neovim-system-config
+
         bat
         fd
         gdu
@@ -21,37 +56,6 @@
 
     environment.variables = {
         EDITOR = "vim";
-    };
-
-    environment.etc."xdg/nvim/sysinit.vim" = {
-      text = ''
-        " Indent with 4 spaces
-            filetype indent on
-            set tabstop=4    " show existing tabs with 4 spaces width
-            set shiftwidth=4 " when indenting with '>', use 4 spacees width
-            set expandtab    " on pressing tab, inssert 4 spaces
-            set autoindent
-            autocmd FileType yaml set indentkeys-=0#
-
-        " Jump to last position
-            autocmd BufReadPost *
-                \ if line("'\"") >= 1 && line("'\"") <= line("$") |
-                \   exe "normal! g`\"" |
-                \ endif
-
-        " Smart case (in)sensitive search
-            :set ignorecase
-            :set smartcase
-
-        " Scrolloff
-            set scrolloff=5
-            " When jumping to end of file, leave 5 lines blank below
-            nnoremap G 5<C-y>G5<C-e>
-
-        " Improved line shifting
-            vnoremap < <gv
-            vnoremap > >gv
-      '';
     };
 
     environment.etc."gitconfig" = {
