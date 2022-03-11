780cef6b57d137a2aadbceccc7bf8ccff37b11f3
^x3ro <git@x3ro.dev>
2022-03-11 16:58:09 +0000

    base config for jarvis

--- configuration/jarvis/configuration.nix
+++ configuration/jarvis/configuration.nix
@@ -22,11 +22,11 @@
     # Define on which hard drive you want to install Grub.
     boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-    # networking.hostName = "nixos"; # Define your hostname.
+    networking.hostName = "jarvis"; # Define your hostname.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
     # Set your time zone.
-    # time.timeZone = "Europe/Amsterdam";
+    time.timeZone = "Europe/Berlin";
 
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -38,65 +38,36 @@
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-    # Select internationalisation properties.
-    # i18n.defaultLocale = "en_US.UTF-8";
-    # console = {
-    #     font = "Lat2-Terminus16";
-    #     keyMap = "us";
-    # };
+    networking.hosts = {
+        #"127.0.0.1" = [ "myapp.local" ];
+    };
 
-    # Enable the X11 windowing system.
-    # services.xserver.enable = true;
-
-
-    
-
-    # Configure keymap in X11
-    # services.xserver.layout = "us";
-    # services.xserver.xkbOptions = "eurosign:e";
-
-    # Enable CUPS to print documents.
-    # services.printing.enable = true;
-
-    # Enable sound.
-    # sound.enable = true;
-    # hardware.pulseaudio.enable = true;
-
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
-
-    # Define a user account. Don't forget to set a password with ‘passwd’.
-    # users.users.jane = {
-    #     isNormalUser = true;
-    #     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    # };
+    environment.shellAliases = {
+    };
 
     # List packages installed in system profile. To search, run:
     # $ nix search wget
-    # environment.systemPackages = with pkgs; [
-    #     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #     wget
-    #     firefox
-    # ];
+    environment.systemPackages = with pkgs; [
+        # Example: Build vscode with extra dependencies
+        #(vscode.overrideAttrs (oldAttrs: {
+        #  buildInputs = oldAttrs.buildInputs ++ [ polkit ];
+        #}))
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #     enable = true;
-    #     enableSSHSupport = true;
-    # };
+        bind
+    ];
 
-    # List services that you want to enable:
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    # (generate hashed passwords with `mkpasswd -m sha512`)
+    users.users.root = { hashedPassword = "!"; };
+    users.users.x3ro = {
+        isNormalUser = true;
+        uid = 1000;
+        extraGroups = [
+            "wheel" # Enable ‘sudo’ for the user.
+        ];
+        initialPassword = "changeMe!";
+    };
 
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
