9709104f272a3894c68a799161617a328206b203
^x3ro <git@x3ro.dev>
2022-04-11 21:28:36 +0200

    📦 NEW: Mount SSD "extended"

--- configuration/K1STE/encryption-configuration.nix
+++ configuration/K1STE/encryption-configuration.nix
@@ -27,16 +27,22 @@
         device = "/dev/mapper/extended_crypt";
         fsType = "btrfs";
         #options = [ "subvol=@data" ];
-        encrypted = {
-            enable = true;
-            label = "extended_crypt";
-            blkDev = "/dev/disk/by-uuid/72709ae5-8e3c-4b99-9e13-b384014c1776"; # UUID for encrypted disk
-            # NOTE: At the time this keyfile is accessed, the neededForBoot
-            #       filesystems (see fileSystems.<name?>.neededForBoot) will
-            #       have been mounted under /mnt-root, so the keyfile path
-            #       should usually start with "/mnt-root/".
-            keyFile = "/mnt-root/etc/secrets/initrd/crypto_keyfile.bin";
-        };
+        #encrypted = {
+        #    enable = true;
+        #    label = "extended_crypt";
+        #    blkDev = "/dev/disk/by-uuid/72709ae5-8e3c-4b99-9e13-b384014c1776"; # UUID for encrypted disk
+        #    # NOTE: At the time this keyfile is accessed, the neededForBoot
+        #    #       filesystems (see fileSystems.<name?>.neededForBoot) will
+        #    #       have been mounted under /mnt-root, so the keyfile path
+        #    #       should usually start with "/mnt-root/".
+        #    keyFile = "/mnt-root/etc/secrets/initrd/crypto_keyfile.bin";
+        #};
+    };
+    boot.initrd.luks.devices."extended_crypt" = {
+        allowDiscards = true; # SSD (Potential security risk: https://wiki.archlinux.org/title/Dm-crypt/Specialties#Discard/TRIM_support_for_solid_state_drives_(SSD) )
+        bypassWorkqueues = true; # Improve SSD performance
+        device = "/dev/disk/by-uuid/72709ae5-8e3c-4b99-9e13-b384014c1776"; # UUID for encrypted disk
+        keyFile = "/crypto_keyfile.bin";
     };
 
     # Create swapfile (BTRFS, see https://wiki.archlinux.org/title/Btrfs#Swap_file):
