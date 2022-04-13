4efb728c7f79fee99727379d7ad0232134246331
^x3ro <git@x3ro.dev>
2022-04-13 20:01:35 +0200

    👌 IMPROVE: Remove comment for swapfile (duplicate of `x3ro.btrfs-swapfile` doc)

--- configuration/K1STE/encryption-configuration.nix
+++ configuration/K1STE/encryption-configuration.nix
@@ -49,60 +49,6 @@
         keyFile = "/crypto_keyfile.bin";
     };
 
-    # Create swapfile (BTRFS, see https://wiki.archlinux.org/title/Btrfs#Swap_file):
-    #
-    #     truncate -s 0 /swap/swapfile
-    #
-    # Fix permissions:
-    #
-    #     chmod 600 /swap/swapfile
-    #
-    # Disable "Copy on Write" for swapfile:
-    #
-    #     chattr +C /swap/swapfile
-    #
-    # Disable compression for swapfile:
-    #
-    #     btrfs property set /swap/swapfile compression none
-    #
-    # Fill swapfile
-    #
-    #     dd if=/dev/zero of=/swap/swapfile bs=1M count=2048 status=progress
-    #
-    # Get offset for resume:
-    #
-    #     filefrag -v /swap/swapfile
-    #
-    # In the column `physical_offset`, use the left value of the first line
-    # (the one that ends with "..". In the example below, it is the value
-    # `38912` (surrounded by two stars `**`):
-    #
-    #     Filesystem type is: ef53
-    #     File size of /swapfile is 4294967296 (1048576 blocks of 4096 bytes)
-    #      ext:     logical_offset:        physical_offset: length:   expected: flags:
-    #        0:        0..       0:    **38912..**   38912:      1:            
-    #        1:        1..   22527:      38913..     61439:  22527:             unwritten
-    #        2:    22528..   53247:     899072..    929791:  30720:      61440: unwritten
-    #     ...
-    #
-    # OPTIONAL:
-    #
-    #     mkswap /swap/swapfile
-    #     swapon /swap/swapfile
-    #
-    ## https://discourse.nixos.org/t/how-do-i-set-up-a-swap-file/8323/7?u=x3ro
-    #systemd.services = {
-    #  create-swapfile = {
-    #    serviceConfig.Type = "oneshot";
-    #    wantedBy = [ "swap-swapfile.swap" ];
-    #    script = ''
-    #      if [ -e /swap/swapfile ]; then exit; fi
-    #      ${pkgs.coreutils}/bin/truncate -s 0 /swap/swapfile
-    #      ${pkgs.e2fsprogs}/bin/chattr +C /swap/swapfile
-    #      ${pkgs.btrfs-progs}/bin/btrfs property set /swap/swapfile compression none
-    #    '';
-    #  };
-    #};
     x3ro.btrfs-swapfile = {
         enable = true;
         location = "/swap/SWAPFILE";
