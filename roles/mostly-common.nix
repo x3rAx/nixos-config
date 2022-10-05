f3149e75e85d00cff01803e76e530e05228c898d
^x3ro <git@x3ro.dev>
2022-10-05 23:22:55 +0200

    👌 IMPROVE: Enable German as supported locale

--- roles/mostly-common.nix
+++ roles/mostly-common.nix
@@ -32,7 +32,7 @@
         # consoleKeyMap = "us";
         supportedLocales = [
             "en_US.UTF-8/UTF-8"
-            #"de_DE.UTF-8/UTF-8"
+            "de_DE.UTF-8/UTF-8"
         ];
         defaultLocale = "en_US.UTF-8";
         #extraLocaleSettings = {
