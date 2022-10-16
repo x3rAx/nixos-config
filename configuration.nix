bd10c78165d3e8983c0062d38447111a562170c5
^x3ro <git@x3ro.dev>
2022-10-16 22:35:26 +0200

    📦 NEW: Automatically add symlink to current host config

--- configuration.nix
+++ configuration.nix
@@ -7,4 +7,14 @@ in {
     imports = [
         (lib.toPath "./configuration/${hostname}/configuration.nix")
     ];
+
+    system.activationScripts = {
+        symlinkCurrentHost = {
+            deps = [];
+            text = ''
+                ln -sf "/etc/nixos/configuration/${hostname}" "/etc/nixos/.current-host.tmp"
+                mv /etc/nixos/.current-host.tmp /etc/nixos/current-host # atomically replace it
+            '';
+        };
+    };
 }
