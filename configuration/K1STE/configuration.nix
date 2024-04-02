e27b8cf72a378b14808edcfd3a7996a396194fb5
^x3ro <git@x3ro.dev>
2024-04-02 14:14:24 +0200

    📦 NEW: Enable zsh

--- configuration/K1STE/configuration.nix
+++ configuration/K1STE/configuration.nix
@@ -125,6 +125,9 @@ rec {
     programs.gamemode.enable = true;
     programs.corectrl.enable = true;
 
+
+    programs.zsh.enable = true;
+
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
