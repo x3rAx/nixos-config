8d92e194f3157bd7dfb3026fe0576b0e51cc092d
^x3ro <git@x3ro.dev>
2024-04-02 13:20:31 +0200

    📦 NEW: Enable nix-ld to allow running non-nix binaries
    Enabling nix-ld makes it much more easy to allow editors like Nvim and
    VScode to download and run language server binaries.

--- configuration/K1STE/configuration.nix
+++ configuration/K1STE/configuration.nix
@@ -187,6 +187,9 @@ rec {
     };
 
 
+    programs.nix-ld.enable = true;
+
+
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
     # on your system were taken. It‘s perfectly fine and recommended to leave
