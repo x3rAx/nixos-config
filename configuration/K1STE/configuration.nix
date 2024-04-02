c0c4d4a76050b89e36f98b0301cdbcebbcf84034
^x3ro <git@x3ro.dev>
2024-04-02 14:10:09 +0200

    👌 IMPROVE: Add (disabled) fix for NetworkManager related build error

--- configuration/K1STE/configuration.nix
+++ configuration/K1STE/configuration.nix
@@ -149,6 +149,9 @@ rec {
         #rnix-lsp # Disabled due to CVE-2024-27297 (See: https://discourse.nixos.org/t/nixos-need-help-finding-out-what-is-pulling-in-nix-2-15-3-in-my-config/41103/2)
     ];
 
+    #systemd.services.systemd-udevd.restartIfChanged = false; # TODO: Remove when https://github.com/NixOS/nixpkgs/issues/180175 is fixed
+    #systemd.services.NetworkManager-wait-online.enable = lib.mkForce false; # TODO: Remove when https://github.com/NixOS/nixpkgs/issues/180175 is fixed
+    #systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false; # TODO: Remove when https://github.com/NixOS/nixpkgs/issues/180175 is fixed
 
     # XSecureLock
     environment.etc."systemd/system-sleep/xsecurelock".source =
