38300fefc337e86b06769cb69cfa1954be8599d2
^x3ro <git@x3ro.dev>
2024-07-01 01:37:12 +0200

    🐛 FIX: Remove duplicate section for `systemPackages`

--- roles/desktop-bspwm.nix
+++ roles/desktop-bspwm.nix
@@ -4,12 +4,6 @@
   pkgs,
   ...
 }: rec {
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    xtitle # To get window titles from scripts
-  ];
-
   services.xserver = {
     enable = true;
 
