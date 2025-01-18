35a8f52922ce2b02ec9590c7aa6a6bdcac283f8f
^x3ro <git@x3ro.dev>
2025-01-18 01:05:02 +0100

    🚀 RELEASE: Update flake to NixOS 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
