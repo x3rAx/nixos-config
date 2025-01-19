85c9d767f92ea3e2c2b926201ec8e064d9801878
^x3ro <git@x3ro.dev>
2025-01-19 15:26:06 +0100

    🐛 FIX: Remove speechd override for mumble

--- overlays/mumble/default.nix
+++ overlays/mumble/default.nix
@@ -2,10 +2,10 @@ self: super: {
   mumble = super.mumble.override {
     # Speech support for mumble
     speechdSupport = true;
-    speechd = super.speechd.override {
-      withEspeak = false;
-      withPico = true;
-      withFlite = false;
-    };
+    # speechd = super.speechd.override {
+    #   withEspeak = false;
+    #   withPico = true;
+    #   withFlite = false;
+    # };
   };
 }
