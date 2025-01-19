020e74e25df19dabd8eb8a9c54dd3832bad3a3d8
^x3ro <git@x3ro.dev>
2025-01-19 15:34:07 +0100

    🐛 FIX: Rename deprecated option

--- roles/nvidia.nix
+++ roles/nvidia.nix
@@ -7,7 +7,7 @@
   #nixpkgs.config.allowUnfree = true;
 
   services.xserver.videoDrivers = ["nvidia"];
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
 
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
   # See here:
