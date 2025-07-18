8ac7c643ad71a9d3750a549a421b69e441696821
^x3ro <git@x3ro.dev>
2025-07-18 15:49:55 +0200

    ❄️ REBUILD: [switch] K1STE: Gen. 269 - 25.05.20250715.32a4e87
    Date:          2025-07-18 13:49
    Host:          K1STE
    Generation:    269
    NixOS version: 25.05.20250715.32a4e87
    Kernel:        6.12.38

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "My NixOS configuration";
 
+  nixConfig = {
+    extra-substituters = [
+      #"https://hyprland.cachix.org"
+      #"https://nix-gaming.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
