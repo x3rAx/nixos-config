8819fa4c34f3e7445b11f89165a88c219074afd1
^x3ro <git@x3ro.dev>
2025-11-01 15:25:32 +0100

    👌 IMPROVE: Disable substitutors

--- configuration/K1STE/configuration.nix
+++ configuration/K1STE/configuration.nix
@@ -12,20 +12,20 @@
   # TODO: Figure out why building fails when using this function from `myLib`
   importIfExists = path: lib.optional (builtins.pathExists path) path;
 in rec {
-  nix.settings = {
-    substituters = [
-      #"https://hyprland.cachix.org"
-      #"https://nix-gaming.cachix.org"
-    ];
-    trusted-substituters = [
-      #"https://hyprland.cachix.org"
-      #"https://nix-gaming.cachix.org"
-    ];
-    trusted-public-keys = [
-      #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-      #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-    ];
-  };
+  #nix.settings = {
+  #  substituters = [
+  #    #"https://hyprland.cachix.org"
+  #    #"https://nix-gaming.cachix.org"
+  #  ];
+  #  trusted-substituters = [
+  #    #"https://hyprland.cachix.org"
+  #    #"https://nix-gaming.cachix.org"
+  #  ];
+  #  trusted-public-keys = [
+  #    #"hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+  #    #"nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+  #  ];
+  #};
 
   imports =
     [
