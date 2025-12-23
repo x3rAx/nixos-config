bd036bf25a3e03ca9571afea13f3f0f46d774134
^x3ro <git@x3ro.dev>
2025-12-23 11:51:05 +0100

    📦 NEW: [mod:btrfs] Adapt new btrfs commands for creating swapfiles

--- modules/x3ro/btrfs-swapfile.nix
+++ modules/x3ro/btrfs-swapfile.nix
@@ -22,39 +22,17 @@ in {
         description = ''
           Location of the SWAPFILE.
 
-          Create the file as follows (see https://wiki.archlinux.org/title/Btrfs#Swap_file):
+          Create the file as follows (see https://web.archive.org/web/20251222093538/https://btrfs.readthedocs.io/en/latest/ch-swapfile.html).
 
-              swapfile='/swap/SWAPFILE'
-              truncate -s 0 "$swapfile"
-
-          Fix permissions:
-
-              chmod 600 "$swapfile"
-
-          Disable "Copy on Write" for swapfile:
-
-              chattr +C "$swapfile"
-
-          > [!note]
-          > According to https://forum.endeavouros.com/t/27485/7
-          > since mid 2022, disabling compression is not necessary
-          > anymore (and the command fails).
-          >
-          > I'll still leave this here in case I'll need it again
-          >
-          > > Disable compression for swapfile:
-          > >
-          > >     btrfs property set "$swapfile" compression none
-
-          Fill swapfile
 
               # As a rule of thumb, for hibernation use `ceil(RAM + sqrt(RAM))`
-              size_MB=$((2 * 1024))
-              dd if=/dev/zero of="$swapfile" bs=1M count="$size_MB" status=progress
+              size='2G'
+              swapfile='/swap/SWAPFILE'
+
+              btrfs filesystem mkswapfile --size "$size" "$swapfile"
 
           OPTIONAL:
 
-              mkswap "$swapfile"
               swapon "$swapfile"
         '';
       };
@@ -86,37 +64,10 @@ in {
           The offset where the SWAPFILE is located on the partition.
           This is needed to resume from hibernation.
 
-          To get the offset, follow below instructions:
+          To get the offset, run:
 
-          Usually, the file fragments could be received by using `filefrag -v "$swapfile"`.
-          However, this might result in false offsets on btrfs.
-          (See: https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Hibernation_into_swap_file_on_Btrfs)
-          Instead we compile and use the tool mentioned in the Arch link above:
+              btrfs inspect-internal map-swapfile -r "$swapfile"
 
-              tmpdir=$(mktemp -d --tmpdir)
-              url='https://raw.githubusercontent.com/osandov/osandov-linux/d03f560dcef705c05c026b896b0447b53aa00546/scripts/btrfs_map_physical.c'
-              nix-shell -p wget gcc --run "cd '$tmpdir' && wget '$url' && gcc -O2 -o btrfs_map_physical btrfs_map_physical.c"
-
-          Now run the program to get the file fragments:
-
-              swapfile="/swap/SWAPFILE"
-              "$tmpdir/btrfs_map_physical" "$swapfile" | head
-
-          Use the value from the `PHYSICAL OFFSET` column. In the example table below,
-          the value is 96971939840. It has been surrounded by two stars (`**`) to make
-          it more visible.
-
-              FILE OFFSET     FILE SIZE       EXTENT OFFSET   EXTENT TYPE     LOGICAL SIZE    LOGICAL OFFSET  PHYSICAL SIZE   DEVID   PHYSICAL OFFSET
-              0       134217728       0       regular 134217728       96971939840     134217728       1       **96971939840**
-              134217728       134217728       0       regular 134217728       97830686720     134217728       1       97830686720
-              268435456       134217728       0       regular 134217728       101694009344    134217728       1       101694009344
-              ...
-
-          Finally, we use this physical size to calculate the reume offset:
-          Use for example `qalc` to calculate the offset:
-
-              $ nix-shell -p libqalculate --run 'qalc 96971939840 / 4096'
-              96971939840 / 4096 = 23674790
         '';
       };
 
