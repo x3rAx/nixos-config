24ca281820a922030c3b774fecd55a3f645d0799
^x3ro <git@x3ro.dev>
2026-01-16 23:25:02 +0100

    🐛 FIX: [K1STE] Update network device for WoL

--- configuration/K1STE/configuration.nix
+++ configuration/K1STE/configuration.nix
@@ -170,7 +170,7 @@ in rec {
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;
 
-  networking.interfaces."enp4s0".wakeOnLan.enable = true;
+  networking.interfaces."enp5s0".wakeOnLan.enable = true;
 
   programs.gamemode.enable = true;
   programs.corectrl.enable = true;
