39886e63f908d36864fbdaefd6e468bbea8656fc
^x3ro <git@x3ro.dev>
2026-01-17 14:56:54 +0100

    🐛 FIX: Copy module config files into new generation

--- modules/default.nix
+++ modules/default.nix
@@ -1,7 +1,8 @@
-{
+{myLib, ...}: rec {
   imports = [
     ./ios-usb.nix
     ./programs/lutris.nix
     ./programs/neovim.nix
   ];
+  system.systemBuilderCommands = myLib.createCopyExtraConfigFilesScript imports;
 }
