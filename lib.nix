62d46e5ca2a8086ca0b1a36b71b3d52af5759507
^x3ro <git@x3ro.dev>
2026-01-17 21:58:29 +0100

    ‼️ BREAKING: Remove deprecated `lib`

--- lib.nix
+++
@@ -1,9 +0,0 @@
-abort "
-    MyLib moved to module arguments and using `import` will no longer work.
-    Please require `myLib` through module arguments instead:
-    
-        { myLib, ...}:
-        {
-            # ...
-        }
-"
